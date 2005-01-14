d3eb815f870557ae86d716537f1af68010b01c96
Andrew Maltsev <am@ejelta.com>
2005-01-14 02:42:19 +0000

    Import of AMALTSEV/XAO-ImageCache-1 from CPAN.
    gitpan-cpan-distribution: XAO-ImageCache
    gitpan-cpan-version:      1
    gitpan-cpan-path:         AMALTSEV/XAO-ImageCache-1.tar.gz
    gitpan-cpan-author:       AMALTSEV
    gitpan-cpan-maturity:     released

---
+++ test.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use XAO::TestUtils;
+
+eval "use XAO::ImageCache";
+if($@) { die "Can't load XAO::ImageCache - call as ``perl -Mblib $0'' ($@)\n" }
+
+if(@ARGV) {
+    XAO::TestUtils::xao_test(@ARGV);
+}
+else {
+    XAO::TestUtils::xao_test_all('testcases');
+}
